2cf0bfd5a80631a206f208b1ff614d3d0798b63b
abeeramad <abeer875abeer@gmail.com>
2021-12-01 12:03:39 +0200

    first commit

---
+++ app/src/main/java/com/example/abeerem/addrecored.java
@@ -0,0 +1,14 @@
+package com.example.abeerem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addrecored extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addrecored);
+    }
+}
